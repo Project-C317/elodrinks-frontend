98adc320918275965eb9265283c6a40d6587b6e3
wilicarol <wili.carol0235@gmail.com>
2025-06-18 16:20:50 -0300

    feat: Teste do contador para o carrinho e fixação do ícone

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,51 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+
 export default function Header() {
+  const [cartSize, setCartSize] = useState(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const carrinho = JSON.parse(localStorage.getItem("carrinho") || "[]");
+    setCartSize(carrinho.length);
+  }, []);
+
+  const goToCarrinho = () => {
+    navigate("/carrinho");
+  };
   return (
     <header className='menu-fixo'>
       <nav>
+        {/* Carrinho no canto superior esquerdo */}
+        <div
+          style={{
+            position: "fixed",
+            top: "16px",
+            left: "16px",
+            cursor: "pointer",
+            zIndex: 1001, // acima do menu
+          }}
+          onClick={goToCarrinho}
+        >
+          <img src="carrinho.svg" alt="Carrinho" width={25} height={40} />
+          {cartSize > 0 && (
+            <span
+              style={{
+                position: "absolute",
+                top: "-5px",
+                right: "-10px",
+                backgroundColor: "#c49b66",
+                color: "white",
+                borderRadius: "100%",
+                padding: "0.01rem 0.4rem",
+                fontSize: "0.75rem",
+              }}
+            >
+              {cartSize}
+            </span>
+          )}
+        </div>
         <a href='#nossos-servicos'>NOSSOS SERVIÇOS</a>
         <a href='#cardapio'>CARDAPIO</a>
         <a href='#home'>
